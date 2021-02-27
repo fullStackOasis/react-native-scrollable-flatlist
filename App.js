05f7a863f96f9116d391b529ea27e762632071d1
fullStackOasis <fullstackdev@fullstackoasis.com>
2021-02-27 07:23:50 -0500

    our FlatList goes up to 11. This lets you scroll.

--- App.js
+++ App.js
@@ -17,7 +17,40 @@ const DATA = [
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
     title: 'Third Item',
   },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d74',
+    title: 'Fourth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d75',
+    title: 'Fifth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d76',
+    title: 'Sixth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d77',
+    title: 'Seventh Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d78',
+    title: 'Eighth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d79',
+    title: 'Ninth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d70',
+    title: 'Tenth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d80',
+    title: 'Eleventh Item',
+  },
 ];
+
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -33,6 +66,7 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
+        style={styles.fl}
         data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.id}
@@ -43,13 +77,16 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex : 1,
     marginTop: StatusBar.currentHeight || 0,
+  },
+  fl : {
+    flex : 1
   },
   item: {
     backgroundColor: '#f9c2ff',
     padding: 20,
-    marginVertical: 8,
+    marginVertical: 20,
     marginHorizontal: 16,
   },
   title: {
